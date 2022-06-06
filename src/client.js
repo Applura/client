84121cc59512b065f5732135dc7a0319aab51278
Gabriel SULLICE <gabriel@sullice.com>
2022-06-06 15:57:03 -0600

    fix Client function to use "this"

--- src/client.js
+++ src/client.js
@@ -34,103 +34,98 @@ async function request(url, options) {
 }
 
 export function bootstrap() {
-    console.assert(window, 'boostrap error: must be called from within a browser context');
+    console.assert(!!window, 'boostrap error: must be called from within a browser context');
     const link = window.document.querySelector('head link[rel*="alternate"][type="application/vnd.api+json"]');
-    console.assert(link, 'bootstrap error: missing initial resource link');
+    console.assert(!!link, 'bootstrap error: missing initial resource link');
     return new Client(link.getAttribute('href'));
 }
 
 export default function Client(initialURL) {
-    let start;
-    let tracking, trackingErrors;
-    let started = new Promise((resolve) => {
-        start = () => {
-            tracking = true;
-            resolve();
-        }
-    })
+    let tracking;
     let stopped = false;
     let send;
-    let lastResource, lastProblem, lastResponse;
-    let baseURL = new URL(initialURL);
-    return {
-        start: async function* () {
-            if (!trackingErrors) {
-                throw new UsageError('must await problems() generator before awaiting resources with start()');
+    let lastURL, lastResource, lastProblem, lastResponse;
+    let baseURL = lastURL = ensureURL(initialURL)
+
+    this.start = async function* () {
+        tracking = true;
+        let first = true;
+        while (tracking) {
+            yield new Promise((resolve) => {
+                send = () => {
+                    resolve({ resource: lastResource, problem: lastProblem });
+                };
+            });
+            if (first) {
+                this.follow(initialURL);
+                first = false;
             }
-            start();
-            let first = true;
-            while (tracking) {
-               yield new Promise((resolve) => {
-                   send = () => {
-			   resolve({ resource: lastResource, problem: lastProblem });
-		   };
-               });
-               if (first) {
-                   this.follow(initialURL);
-               }
-            }
-        },
-        stop: function () {
-            if (stopped) {
-                return;
-            }
-            tracking = false;
-            send();
-            stopped = true;
-        },
-        follow: async function (link, options) {
-            const url = ensureURL(link, baseURL);
-            const response = lastResponse = await request(url, options);
-            if (response.status === 204) {
-                return true;
-            }
-            if (!response.headers.has('content-type')) {
-                lastProblem = new MissingContentTypeError('the server responded without specifying a MIME type via the content-type HTTP response header', {response});
-                send();
-            }
-            const mimeType = response.headers.get('content-type');
-            if (!mimeType.startsWith('application/vnd.api+json')) {
-                lastProblem = new UnexpectedContentTypeError(response, `the server responded in with an unrecognizable media type: ${mimeType}`, {response});
-                send();
-            }
-            let doc
-            try {
-                doc = await response.json();
-            } catch (e) {
-                lastProblem = new UnexpectedContentError(`could not parse response as JSON despite the content-type header claiming to be ${mimeType}: ${e.reason}`, { response, cause: e });
-                send();
-                return false;
-            }
-            if (response.ok) {
-                try {
-                    lastResource = parse(doc);
-                    send();
-                } catch (e) {
-                    throw new ImplementationError('could not parse JSON:API document', {cause: e});
-                }
-                return true;
-            }
-            const errorDetails = (doc.errors || []).filter((e) => e.detail).map((e) => `detail: ${e.detail}`)
-            if (response.status >= 400 && response.status <= 499) {
-                lastProblem = new RequestError(['request error', `${response.status} ${response.statusText}`, ...errorDetails].join(': '), {doc, response})
-                send();
-            } else if (response.status >= 500 && response.status <= 599) {
-                lastProblem = new ServerError(response, ['response error', `${response.status} ${response.statusText}`, ...errorDetails], {doc, response});
-                send();
-            } else {
-                throw new ImplementationError(`unhandled response type: ${response.status} ${response.statusText}`);
-            }
-            return false;
-        },
-        resource: function () {
-            return lastResource;
-        },
-        response: function () {
-            return lastResponse;
-        },
-        refresh: function () {
-            this.follow(lastURL);
         }
     };
+
+    this.stop = function () {
+        if (stopped) {
+            return;
+        }
+        tracking = false;
+        send();
+        stopped = true;
+    };
+
+    this.follow = async function (link, options) {
+        const url = lastURL = ensureURL(link, baseURL);
+        const response = lastResponse = await request(url, options);
+        if (response.status === 204) {
+            return true;
+        }
+        if (!response.headers.has('content-type')) {
+            lastProblem = new MissingContentTypeError('the server responded without specifying a MIME type via the content-type HTTP response header', {response});
+            send();
+        }
+        const mimeType = response.headers.get('content-type');
+        if (!mimeType.startsWith('application/vnd.api+json')) {
+            lastProblem = new UnexpectedContentTypeError(response, `the server responded in with an unrecognizable media type: ${mimeType}`, {response});
+            send();
+        }
+        let doc
+        try {
+            doc = await response.json();
+        } catch (e) {
+            lastProblem = new UnexpectedContentError(`could not parse response as JSON despite the content-type header claiming to be ${mimeType}: ${e.reason}`, { response, cause: e });
+            send();
+            return false;
+        }
+        if (response.ok) {
+            try {
+                lastResource = parse(doc);
+                send();
+            } catch (e) {
+                throw new ImplementationError('could not parse JSON:API document', {cause: e});
+            }
+            return true;
+        }
+        const errorDetails = (doc.errors || []).filter((e) => e.detail).map((e) => `detail: ${e.detail}`)
+        if (response.status >= 400 && response.status <= 499) {
+            lastProblem = new RequestError(['request error', `${response.status} ${response.statusText}`, ...errorDetails].join(': '), {doc, response})
+            send();
+        } else if (response.status >= 500 && response.status <= 599) {
+            lastProblem = new ServerError(response, ['response error', `${response.status} ${response.statusText}`, ...errorDetails], {doc, response});
+            send();
+        } else {
+            throw new ImplementationError(`unhandled response type: ${response.status} ${response.statusText}`);
+        }
+        return false;
+    }
+
+    this.resource = function () {
+        return lastResource;
+    }
+
+    this.response = function () {
+        return lastResponse;
+    }
+
+    this.refresh = function () {
+        return this.follow(lastURL);
+    }
 }

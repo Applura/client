171f75f68f5efda3312ac049218e4e130ed6f270
Gabriel Sullice <gabriel@sullice.com>
2025-03-11 13:48:56 +0100

    convert: every error to a problem details object

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 import Client, { bootstrap } from "./src/client.js";
-import {
-  ImplementationError,
-  LibraryError,
-  MissingContentTypeError,
-  RequestError,
-  ResponseError,
-  ServerError,
-  UnexpectedContentError,
-  UnexpectedContentTypeError,
-  UnprocessableResponseError,
-  UsageError,
-} from "./src/errors.js";
-
 export default Client;
 export { bootstrap };
-export {
-  ImplementationError,
-  LibraryError,
-  MissingContentTypeError,
-  RequestError,
-  ResponseError,
-  ServerError,
-  UnexpectedContentError,
-  UnexpectedContentTypeError,
-  UnprocessableResponseError,
-  UsageError,
-};

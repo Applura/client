1f38b87eb5083ae9d9c723fa9fa4354b707208b5
Serhii Kostiukevych <serhii.kostiukevych@lembergsolutions.com>
2025-06-23 15:09:59 +0300

    Update rollup config

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,8 +1,6 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 
 module.exports = {
-  input: "src/cli.js",
-  output: [{ file: "bin/cli.js", format: "es" }],
   plugins: [
     nodeResolve({ resolveOnly: ["npm:@applura/ouroboros"] }),
   ],
